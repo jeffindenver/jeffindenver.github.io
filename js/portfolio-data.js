9143dc55321af8043a1ccdecb1855c9c5808a409
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-05-17 11:42:16 -0500

    added files for week 3

---
+++ js/portfolio-data.js
@@ -0,0 +1,76 @@
+function Data() {
+  "use strict";
+
+  this.getDiagramList = function () {
+    return [{
+        href: "nosql-data-diagrams/nosql-data-structure.png",
+        src: "./images/diagrams/diagram_thumb_one.png",
+        alt: "JSON example"
+      },
+      {
+        href: "nosql-data-diagrams/assignment1.4-ord.png",
+        src: "./images/diagrams/diagram_thumb_two.png",
+        alt: "UML diagram"
+      },
+      {
+        href: "nosql-data-diagrams/NoSQL-data-structure-assignment2.2.png",
+        src: "./images/diagrams/diagram_thumb_three.png",
+        alt: "JSON example"
+      },
+      {
+        href: "nosql-data-diagrams/ORD-assignment2.2.png",
+        src: "./images/diagrams/diagram_thumb_four.png",
+        alt: "UML diagram"
+      },
+      {
+        href: "nosql-data-diagrams/NoSQL-data-structure-assignment2.3.png",
+        src: "./images/diagrams/diagram_thumb_five.png",
+        alt: "JSON example"
+      },
+      {
+        href: "nosql-data-diagrams/ORD-assignment2.3.png",
+        src: "./images/diagrams/diagram_thumb_six.png",
+        alt: "UML diagram"
+      },
+      {
+        href: "nosql-data-diagrams/NoSQL-data-structure-assignment3.2.png",
+        src: "./images/diagrams/diagram_thumb_eight.png",
+        alt: "JSON example"
+      },
+      {
+        href: "nosql-data-diagrams/ORD-assignment3.2.png",
+        src: "./images/diagrams/diagram_thumb_seven.png",
+        alt: "UML diagram"
+      },
+      {
+        href: "nosql-data-diagrams/NoSQL-data-structure-assignment3.3.png",
+        src: "./images/diagrams/diagram_thumb_nine.png",
+        alt: "JSON example"
+      },
+      {
+        href: "nosql-data-diagrams/ORD-assignment3.3.png",
+        src: "./images/diagrams/diagram_thumb_ten.png",
+        alt: "UML diagram"
+      }
+    ];
+  };
+
+  this.getPresentationList = function () {
+    return [{
+        src: "./restful-api-presentations/rest-video.mp4",
+        alt: "video presentation. No audio",
+        width: "426"
+      },
+      {
+        src: "./restful-api-presentations/restful-api-video.mp4",
+        alt: "video presentation. No audio",
+        width: "426"
+      },
+      {
+        src: "./restful-api-presentations/soap-video.mp4",
+        alt: "video presentation, No audio",
+        width: "426"
+      }
+    ]
+  };
+}
--- js/portfolio.js
+++ js/portfolio.js
@@ -14,41 +14,9 @@ let background = new Background();
 background.draw();
 let animationID = background.startTimers();
 
-let diagramList = [{
-  href: "nosql-data-diagrams/nosql-data-structure.png",
-  src: "./images/diagram_thumb_one.png",
-  alt: "JSON example"
-}, {
-  href: "nosql-data-diagrams/assignment1.4-ord.png",
-  src: "./images/diagram_thumb_two.png",
-  alt: "UML Diagram"
-}, {
-  href: "nosql-data-diagrams/NoSQL-data-structure-assignment2.2.png",
-  src: "./images/diagram_thumb_three.png",
-  alt: "JSON example"
-}, {
-  href: "nosql-data-diagrams/ORD-assignment2.2.png",
-  src: "./images/diagram_thumb_four.png",
-  alt: "UML Diagram"
-}, {
-  href: "nosql-data-diagrams/NoSQL-data-structure-assignment2.3.png",
-  src: "./images/diagram_thumb_five.png",
-  alt: "JSON example"
-}, {
-  href: "nosql-data-diagrams/ORD-assignment2.3.png",
-  src: "./images/diagram_thumb_six.png",
-  alt: "UML Diagram"
-}];
-
-let presentationList = [{
-  src: "./restful-api-presentations/rest-video.mp4",
-  alt: "video presentation. No audio",
-  width: "426"
-}, {
-  src: "./restful-api-presentations/restful-api-video.mp4",
-  alt: "video presentation. No audio",
-  width: "426"
-}];
+const data = new Data();
+const diagramList = data.getDiagramList();
+const presentationList = data.getPresentationList();
 
 $(function () {
   let diagrams = new ImageGrid();
