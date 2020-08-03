6a291d4b698115b65600d8cdd8fad3b6908b068a
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-08-03 00:48:55 -0500

    added video presentation

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -108,6 +108,11 @@ function Data() {
         src: "./restful-api-presentations/automated-testing-video.mp4",
         alt: "video presentation, no audio",
         width: "426"
+      },
+      {
+        src: "./restful-api-presentations/continuous-integration-video.mp4",
+        alt: "video presentation, no audio",
+        width: "426"
       }
     ]
   };
