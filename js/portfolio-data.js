e6399fd9f76e1c5831541c9da3b82a3eaa136e05
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-08-09 16:16:05 -0500

    added pager-rotation video

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -113,6 +113,11 @@ function Data() {
         src: "./restful-api-presentations/continuous-integration-video.mp4",
         alt: "video presentation, no audio",
         width: "426"
+      },
+      {
+        src: "./restful-api-presentations/pager-rotation-duty-video.mp4",
+        alt: "video presentation, no audio",
+        width: "426"
       }
     ]
   };
