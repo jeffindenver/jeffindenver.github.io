b271dc3dd1e95bf1d7d462019402d487836472b0
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-08-23 20:07:00 -0500

    added week 7 video

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -123,6 +123,11 @@ function Data() {
         src: "./restful-api-presentations/approval-process-video.mp4",
         alt: "video presentation, no audio",
         width: "426"
+      },
+      {
+        src: "./restful-api-presentations/technology-versus-business-video.mp4",
+        alt: "video presentation, no audio",
+        width: "426"
       }
 
     ]
